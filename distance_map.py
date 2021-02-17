93a8a2edd6b51a5b2e287bb0ee9be28577547c94
SherryLuXM <39537416+SherryLuXM@users.noreply.github.com>
2021-02-17 17:05:07 -0500

    Update distance_map.py

--- distance_map.py
+++ distance_map.py
@@ -27,13 +27,14 @@ from typing import Dict
 
 
 class DistanceMap:
-    """Lets client code store and look up the distance betweeen any two
-    cities.
+    """Lets client code store and look up the distance betweeen any two cities.
+
     === Attributes ===
     _distances: A dictionary that keeps track of the distance between two
         cities, each key item is a string with one city name, and its value is a dictionary which takes another city
-        as destination and an integer which is the distance between them. Keep in mind the distance from city A to city
-        B could be different than the distance from city B to city A!
+        as destination and an integer which is the distance from the first city to the second city.
+        The distance from city A to city B could be different than the distance from city B to city A.
+        If the distance from city A to city B is recorded, the distance from city B to city A must be available.
     === Representation Invariants ===
     - The distance between cities must not be negative.
     === Sample Usage ===
@@ -41,7 +42,6 @@ class DistanceMap:
     >>> d.add_distance('Toronto', 'Montreal', 10)
     >>> d.distance('Toronto', 'Montreal')
     10
-    >>> d.add_distance('Toronto', 'Ottawa')
     >>> d.distance('Toronto', 'Ottawa')
     -1
     """
@@ -52,26 +52,37 @@ class DistanceMap:
         storage."""
         self._distances = {}
 
-    def add_distance(self, city_a: str, city_b: str, distance: int = -1) -> None:
-        """Add the distance from <city_a> to <city_b> to our private dictionary.
-        If no distance parameter is passed, add -1 as the distance.
+    def add_distance(self, city_a: str, city_b: str, distance1: int, distance2: int = -1) -> None:
+        """Add <distance1> and <distance2> for <city_a> and <city_b> to <self._distances>.
+        If no value is passed to <distance2>, then <distance2> is the same as <distance1>, unless <self._distances>
+        already stores value for distance from city_b to city_a.
 
         >>> d = DistanceMap()
         >>> d.add_distance('Edmonton', 'Toronto', 40)
-        >>> d.add_distance('Toronto', 'Calgary', 45)
         >>> d.distance('Edmonton', 'Toronto')
         40
-        >>> d.distance('Toronto', 'Calgary')
+        >>> d.distance('Toronto', 'Edmonton')
+        40
+        >>> d.add_distance('Toronto', 'Edmonton', 45)
+        >>> d.distance('Edmonton', 'Toronto')
+        40
+        >>> d.distance('Toronto', 'Edmonton')
         45
-        >>> d.add_distance('Toronto', 'Ottawa')
-        >>> d.distance('Toronto', 'Ottawa')
-        -1
         """
-        try:
-            self._distances[city_a][city_b] = distance
-        except KeyError:
-            self._distances[city_a] = {}
-            self._distances[city_a][city_b] = distance
+        if distance2 == -1:  # check if <distance2> is not passed
+            distance2 = distance1
+        # record <distance1> to self._distances
+        if city_a in self._distances:  # check if <self._distances> already has values for <city_a>
+            self._distances[city_a][city_b] = distance1
+        else:
+            self._distances[city_a] = {city_b: distance1}
+        # record <distance2> to self._distances
+        if city_b in self._distances:
+            # check if the distance from <city_b> to <city_a> is already stored in <self._distances>
+            if city_a not in self._distances[city_b]:
+                self._distances[city_b][city_a] = distance2
+        else:
+            self._distances[city_b] = {city_a: distance2}
 
     def distance(self, city_a: str, city_b: str) -> int:
         """Return the distance from <city_a> to <city_b> stored in _distances.
